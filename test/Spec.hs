8673c1347e9312ff24f735406fe3dc130820fbe0
Dmitrii Kovanikov <kovanikov@gmail.com>
2021-12-13 08:56:19 +0000

    [#4] Add tests (#9)
    * [#4] Add tests
    
    Resolves #4
    
    * Remove macOS from CI (because no docker)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,37 @@
 module Main (main) where
 
+import Control.Exception (bracket)
+import System.IO (hSetEncoding, utf8)
+import Test.Hspec (Spec, describe, hspec)
+
+import Piece (mkAppEnv)
+import Piece.App.Env (Env (..))
+import Piece.App.Monad (AppEnv)
+import Piece.Db.Schema (prepareDb)
+import Piece.Effects.Log (runAppLogIO_)
+import Test.Db (dbSpec)
+
+import qualified Data.Pool as Pool
+
+
+hspecTests :: AppEnv -> Spec
+hspecTests env = describe "Unit tests" $ do
+    dbSpec env
+
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: String)
+main = bracket
+    mkAppEnv
+    (\Env{..} -> Pool.destroyAllResources envDbPool)
+    runTests
+  where
+    runTests :: AppEnv -> IO ()
+    runTests env = do
+        -- fix terminal encoding
+        hSetEncoding stdout utf8
+        hSetEncoding stderr utf8
+
+        -- setup DB tables
+        runAppLogIO_ env prepareDb
+
+        -- run all tests
+        hspec $ hspecTests env
